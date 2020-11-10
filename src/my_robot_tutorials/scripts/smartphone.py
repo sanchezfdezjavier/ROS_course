53024077677f8eecac7a5254623346c645642a24
Javier Sanchez Fernandez <sanchezfdez.javier@gmail.com>
2020-11-10 18:01:28 +0100

    lidar pointcloud2 input to xyz

--- src/my_robot_tutorials/scripts/smartphone.py
+++ src/my_robot_tutorials/scripts/smartphone.py
@@ -5,7 +5,7 @@ from std_msgs.msg import String
 
 def callback_receive_radio_data(msg):
     rospy.loginfo("Message received: ")
-    rospy.loginfo("msg")
+    rospy.loginfo(msg)
 
 if __name__ == '__main__':
     rospy.init_node('smartphone')
